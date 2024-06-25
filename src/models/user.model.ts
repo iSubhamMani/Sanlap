ef1acd8532a432c46887472142a417ebf0a45359
Subham Mani <manisubham09@gmail.com>
2024-06-25 11:30:13 +0530

    Add user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,19 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface User extends Document {
+  _id: string;
+  email: string;
+  displayName: string;
+  photoURL: string;
+}
+
+const userSchema: Schema<User> = new Schema<User>({
+  _id: { type: String, required: true },
+  email: { type: String, required: true },
+  displayName: { type: String, required: true },
+  photoURL: { type: String, required: true },
+});
+
+export const UserModel =
+  (mongoose.models.User as mongoose.Model<User>) ||
+  mongoose.model("User", userSchema);
