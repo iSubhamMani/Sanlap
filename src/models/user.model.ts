9c2519f5eddf03f963643180d99198b953c091f2
Subham Mani <manisubham09@gmail.com>
2024-06-28 19:53:35 +0530

    add timestamps to user model schema

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -7,12 +7,15 @@ export interface User extends Document {
   photoURL: string;
 }
 
-const userSchema: Schema<User> = new Schema<User>({
-  _id: { type: String, required: true },
-  email: { type: String, required: true },
-  displayName: { type: String, required: true },
-  photoURL: { type: String, required: true },
-});
+const userSchema: Schema<User> = new Schema<User>(
+  {
+    _id: { type: String, required: true },
+    email: { type: String, required: true },
+    displayName: { type: String, required: true },
+    photoURL: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 export const UserModel =
   (mongoose.models.User as mongoose.Model<User>) ||
