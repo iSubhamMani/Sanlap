c7299a3a307d495d8c43cb44b97fcd71295d83cd
Subham Mani <manisubham09@gmail.com>
2024-06-28 19:51:49 +0530

    Add API route for rejecting invitations

---
+++ src/app/api/reject-invitation/route.ts
@@ -0,0 +1,34 @@
+import { connectDB } from "@/lib/db";
+import { InvitationModel } from "@/models/invitation.model";
+import { InvitationRequest } from "@/types/InvitationRequest";
+import { ApiError } from "@/utils/ApiError";
+import { ApiSuccess } from "@/utils/ApiSuccess";
+
+export async function POST(req: Request) {
+  await connectDB();
+
+  try {
+    const { sender, recipient } = (await req.json()) as InvitationRequest;
+
+    if (!sender || !recipient) {
+      return Response.json(
+        new ApiError(400, "Sender and receiver is required"),
+        { status: 400 }
+      );
+    }
+
+    const response = await InvitationModel.deleteOne({ sender, recipient });
+
+    if (!response) {
+      return Response.json(new ApiError(500, "Error rejecting invitation"), {
+        status: 500,
+      });
+    }
+
+    return Response.json(new ApiSuccess(200, "Invitation Rejected", response), {
+      status: 200,
+    });
+  } catch (error: any) {
+    return Response.json(new ApiError(500, error.message), { status: 500 });
+  }
+}
