2671d9c6f5d2c840d2b42a6570158a46c7b691b9
Subham Mani <manisubham09@gmail.com>
2024-06-28 19:53:08 +0530

    Refactor invitation model schema and add timestamps

--- src/models/invitation.model.ts
+++ src/models/invitation.model.ts
@@ -6,11 +6,14 @@ export interface Invitation extends Document {
   status: "pending" | "accepted" | "unsent";
 }
 
-const invitationSchema: Schema<Invitation> = new Schema<Invitation>({
-  sender: { type: Schema.Types.String, ref: "User", required: true },
-  recipient: { type: Schema.Types.String, required: true },
-  status: { type: String, required: true },
-});
+const invitationSchema: Schema<Invitation> = new Schema<Invitation>(
+  {
+    sender: { type: Schema.Types.String, ref: "User", required: true },
+    recipient: { type: Schema.Types.String, required: true },
+    status: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 export const InvitationModel =
   (mongoose.models.Invitation as mongoose.Model<Invitation>) ||
