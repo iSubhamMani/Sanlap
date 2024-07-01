3c263ef790a7698614445c35b619c91b55c2eb82
Subham Mani <manisubham09@gmail.com>
2024-07-01 12:57:27 +0530

    Add ChatUser component to display user information in chat

---
+++ src/components/ChatUser.tsx
@@ -0,0 +1,37 @@
+import { UserRound } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Conversation } from "./Conversations";
+import { UserInfo } from "@/lib/features/user/userSlice";
+
+const ChatUser = ({
+  conversation,
+  currentUser,
+}: {
+  conversation: Conversation;
+  currentUser: UserInfo | null;
+}) => {
+  const { members } = conversation;
+  const otherMember = members.find((member) => member._id !== currentUser?.uid);
+
+  if (!otherMember || !currentUser) {
+    return null;
+  }
+
+  return (
+    <div className="py-4 my-2 px-4 flex justify-between items-center gap-3 cursor-pointer rounded-sm hover:bg-muted transition ease-in-out duration-200">
+      <div className="flex items-center gap-3">
+        <Avatar>
+          <AvatarImage src={otherMember.photoURL} />
+          <AvatarFallback>
+            <UserRound />
+          </AvatarFallback>
+        </Avatar>
+        <div className="flex flex-col items-start">
+          <p className="text-lg font-medium">{otherMember.displayName}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatUser;
