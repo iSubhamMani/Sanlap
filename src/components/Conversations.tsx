d4e56a3d06f25f96312cc224d7e8c84e8d03fda6
Subham Mani <manisubham09@gmail.com>
2024-07-01 12:57:39 +0530

    Add Conversations component to display chat conversations

---
+++ src/components/Conversations.tsx
@@ -0,0 +1,52 @@
+import axios from "axios";
+import ChatUser from "./ChatUser";
+import { useEffect, useState } from "react";
+import { User } from "@/models/user.model";
+import { useAppSelector } from "@/lib/hooks";
+
+export interface Conversation {
+  _id: string;
+  members: User[];
+  name?: string;
+  profilePic?: string;
+  lastMessageAt: Date;
+}
+
+const Conversations = () => {
+  const [conversations, setConversations] = useState([]);
+  const { info } = useAppSelector((state) => state.user);
+
+  useEffect(() => {
+    getAllConversations();
+  }, []);
+
+  const getAllConversations = async () => {
+    try {
+      const response = await axios.get("/api/conversations", {
+        headers: {
+          Authorization: `${localStorage.getItem("token")}`,
+        },
+      });
+
+      if (response.data?.success) {
+        setConversations(response.data.data);
+      }
+    } catch (error) {
+      console.log("Error fetching conversations: ", error);
+    }
+  };
+
+  return (
+    <div className="flex-1 flex flex-col">
+      {conversations?.map((conversation: Conversation) => (
+        <ChatUser
+          key={conversation._id}
+          currentUser={info}
+          conversation={conversation}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Conversations;
