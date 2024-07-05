b784a5ee304e5ef2ec4f00b741840ab8e3e9ebc0
Subham Mani <manisubham09@gmail.com>
2024-07-05 12:23:55 +0530

    add pagination

--- src/components/MessagesContainer.tsx
+++ src/components/MessagesContainer.tsx
@@ -2,9 +2,10 @@
 
 import { User } from "@/models/user.model";
 import axios from "axios";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import ChatMessage from "./ChatMessage";
 import { pusherClient } from "@/lib/pusher";
+import { MESSAGES_PAGE_SIZE } from "@/lib/constants";
 
 export interface CustomMessage {
   _id: string;
@@ -17,27 +18,48 @@ export interface CustomMessage {
 }
 
 const MessagesContainer = ({ conversationId }: { conversationId: string }) => {
+  const memoizedConversationId = useMemo(
+    () => conversationId,
+    [conversationId]
+  );
+
   const [messages, setMessages] = useState<CustomMessage[]>([]);
+  const [page, setPage] = useState<number>(1);
+
+  const memoizedPage = useMemo(() => page, [page]);
 
   const getAllMessages = useCallback(async () => {
-    if (!conversationId) return;
+    if (!memoizedConversationId) return;
     try {
-      const response = await axios.get(`/api/all-messages/${conversationId}`, {
-        headers: {
-          Authorization: `${localStorage.getItem("token")}`,
-        },
-      });
+      const response = await axios.get(
+        `/api/all-messages/${memoizedConversationId}?page=${memoizedPage}&pageSize=${MESSAGES_PAGE_SIZE}`,
+        {
+          headers: {
+            Authorization: `${localStorage.getItem("token")}`,
+          },
+        }
+      );
       if (response.data?.success) {
-        setMessages(response.data.data);
+        setMessages((prevMessages) => [
+          ...prevMessages,
+          ...response.data.data.messages,
+        ]);
       }
     } catch (error) {
       console.log("Error fetching messages: ", error);
     }
-  }, [conversationId]);
+  }, [memoizedPage]);
 
   useEffect(() => {
     getAllMessages();
   }, [getAllMessages]);
+
+  useEffect(() => {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: "smooth",
+    });
+  }, [messages]);
 
   useEffect(() => {
     pusherClient.subscribe(`messages-${conversationId}`);
@@ -54,10 +76,10 @@ const MessagesContainer = ({ conversationId }: { conversationId: string }) => {
     };
   }, []);
 
-  if (!conversationId) return null;
+  if (!memoizedConversationId) return null;
 
   return (
-    <div className="flex-1 overflow-auto">
+    <div className="flex-1">
       <div className="grid gap-6 px-6 py-8">
         {messages?.map((message) => (
           <ChatMessage key={message._id} message={message} />
