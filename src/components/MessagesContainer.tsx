6020654a8614befa6e0dd59929a0a0a6e0f264df
Subham Mani <manisubham09@gmail.com>
2024-07-06 12:03:49 +0530

    Add react-infinite-scroll-component for infinite scrolling

--- src/components/MessagesContainer.tsx
+++ src/components/MessagesContainer.tsx
@@ -2,10 +2,12 @@
 
 import { User } from "@/models/user.model";
 import axios from "axios";
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import ChatMessage from "./ChatMessage";
 import { pusherClient } from "@/lib/pusher";
 import { MESSAGES_PAGE_SIZE } from "@/lib/constants";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Loader from "./Loader";
 
 export interface CustomMessage {
   _id: string;
@@ -28,8 +30,13 @@ const MessagesContainer = ({ conversationId }: { conversationId: string }) => {
 
   const memoizedPage = useMemo(() => page, [page]);
 
-  const getAllMessages = useCallback(async () => {
+  const [shouldScroll, setShouldScroll] = useState<boolean>(true);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [hasMore, setHasMore] = useState<boolean>(false);
+
+  const loadMessages = useCallback(async () => {
     if (!memoizedConversationId) return;
+    setLoading(true);
     try {
       const response = await axios.get(
         `/api/all-messages/${memoizedConversationId}?page=${memoizedPage}&pageSize=${MESSAGES_PAGE_SIZE}`,
@@ -40,32 +47,40 @@ const MessagesContainer = ({ conversationId }: { conversationId: string }) => {
         }
       );
       if (response.data?.success) {
+        setHasMore(response.data.data.metadata.hasNextPage);
+        setPage((prevPage) => prevPage + 1);
         setMessages((prevMessages) => [
           ...prevMessages,
           ...response.data.data.messages,
         ]);
+        setShouldScroll(false);
       }
     } catch (error) {
       console.log("Error fetching messages: ", error);
+    } finally {
+      setLoading(false);
     }
   }, [memoizedPage]);
 
   useEffect(() => {
-    getAllMessages();
-  }, [getAllMessages]);
+    loadMessages();
+  }, []);
 
   useEffect(() => {
-    window.scrollTo({
-      top: document.documentElement.scrollHeight,
-      behavior: "smooth",
-    });
-  }, [messages]);
+    if (shouldScroll) {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+  }, [messages, shouldScroll]);
 
   useEffect(() => {
     pusherClient.subscribe(`messages-${conversationId}`);
 
     const handleIncomingMessage = async (newMessage: CustomMessage) => {
-      setMessages((prevMessages) => [...prevMessages, newMessage]);
+      setMessages((prevMessages) => [newMessage, ...prevMessages]);
+      setShouldScroll(true);
     };
 
     pusherClient.bind("new-message", handleIncomingMessage);
@@ -80,11 +95,25 @@ const MessagesContainer = ({ conversationId }: { conversationId: string }) => {
 
   return (
     <div className="flex-1">
-      <div className="grid gap-6 px-6 py-8">
-        {messages?.map((message) => (
-          <ChatMessage key={message._id} message={message} />
-        ))}
-      </div>
+      <InfiniteScroll
+        scrollThreshold={0.9}
+        dataLength={messages.length}
+        next={loadMessages}
+        hasMore={hasMore}
+        loader={
+          loading && (
+            <div className="flex justify-center">
+              <Loader />
+            </div>
+          )
+        }
+      >
+        <div className="grid gap-5 px-6 py-8">
+          {messages.map((message) => {
+            return <ChatMessage key={message._id} message={message} />;
+          })}
+        </div>
+      </InfiniteScroll>
     </div>
   );
 };
