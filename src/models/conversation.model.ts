8e856cddcf62ecb902f17302f448ecbc1392a3c3
Subham Mani <manisubham09@gmail.com>
2024-07-09 13:02:08 +0530

    modify conversstion model to include user langs

--- src/models/conversation.model.ts
+++ src/models/conversation.model.ts
@@ -1,13 +1,28 @@
 import mongoose, { Schema } from "mongoose";
 
 interface Conversation {
-  members: string[];
+  members: UserPref[];
   lastMessageAt: Date;
 }
 
+interface UserPref {
+  _id: string;
+  type_in_lang: string;
+  receive_in_lang: string;
+}
+
+const userPrefSchema: Schema<UserPref> = new Schema<UserPref>(
+  {
+    _id: { type: String, ref: "User", required: true },
+    type_in_lang: { type: String, required: true, default: "english" },
+    receive_in_lang: { type: String, required: true, default: "english" },
+  },
+  { _id: false }
+);
+
 const conversationSchema: Schema<Conversation> = new Schema<Conversation>(
   {
-    members: [{ type: Schema.Types.String, ref: "User", required: true }],
+    members: { type: [userPrefSchema], required: true },
     lastMessageAt: { type: Date, required: true, default: Date.now() },
   },
   { timestamps: true }
