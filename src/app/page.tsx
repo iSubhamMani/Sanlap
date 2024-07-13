12a11c6df4b7060886a8f2531ff23dde1e45d625
Subham Mani <manisubham09@gmail.com>
2024-07-13 15:25:06 +0530

    Improve loading page UI with a loader component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 "use client";
 
+import Loader from "@/components/Loader";
+
 export default function LoadingPage() {
   return (
-    <div>
-      <p className="text-4xl text-center">Loading</p>
+    <div className="min-h-[100dvh] flex items-center justify-center">
+      <div className="flex items-center gap-3">
+        <Loader dimensions="w-4 h-4 lg:w-5 lg:h-5" />
+        <p className="text-sm lg:text-base text-center font-medium">
+          Loading...
+        </p>
+      </div>
     </div>
   );
 }
