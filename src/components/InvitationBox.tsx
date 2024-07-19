c4dc2270e91f9b98d84cf55f926e9174030c5222
Subham Mani <manisubham09@gmail.com>
2024-07-19 16:00:20 +0530

    Refactor InvitationBox component and add CustomToast for displaying invitations

--- src/components/InvitationBox.tsx
+++ src/components/InvitationBox.tsx
@@ -19,8 +19,7 @@ import {
 import { pusherClient } from "@/lib/pusher";
 import InvitationCardSkeleton from "./InvitationCardSkeleton";
 import toast from "react-hot-toast";
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import { UserRound } from "lucide-react";
+import CustomToast from "./CustomToast";
 
 const InvitationBox = () => {
   const invitation = useAppSelector((state) => state.invitation);
@@ -58,40 +57,11 @@ const InvitationBox = () => {
       dispatcher(addInvitation(newInvitation));
       const sender = newInvitation.sender;
       toast.custom((t) => (
-        <div
-          className={`${
-            t.visible ? "animate-enter" : "animate-leave"
-          } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
-        >
-          <div className="flex-1 w-0 p-4">
-            <div className="flex items-start">
-              <div className="flex-shrink-0 pt-0.5">
-                <Avatar>
-                  <AvatarImage src={sender?.photoURL} />
-                  <AvatarFallback>
-                    <UserRound />
-                  </AvatarFallback>
-                </Avatar>
-              </div>
-              <div className="ml-3 flex-1">
-                <p className="text-sm font-medium text-gray-900">
-                  {sender?.displayName}
-                </p>
-                <p className="mt-1 text-sm text-gray-500">
-                  sent you an invitation
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="flex border-l border-gray-200">
-            <button
-              onClick={() => toast.dismiss(t.id)}
-              className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-            >
-              Close
-            </button>
-          </div>
-        </div>
+        <CustomToast
+          t={t}
+          user={sender}
+          primaryMessage="sent you an invitation"
+        />
       ));
     };
 
