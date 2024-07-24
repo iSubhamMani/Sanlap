cc795a6cfaa0caab7c6821b4ad6ace17be41bcfe
Subham Mani <manisubham09@gmail.com>
2024-07-24 11:56:50 +0530

    refactor convertDate

--- src/utils/convertDate.ts
+++ src/utils/convertDate.ts
@@ -1,21 +1,19 @@
 import moment from "moment";
 
 export function convertToReadableDate(dateString: string): string {
+  const date = moment(dateString);
   const now = moment();
-  const messageDate = moment(dateString);
-  const diffInMinutes = now.diff(messageDate, "minutes");
 
-  if (diffInMinutes < 1) {
-    return "just now";
-  } else if (diffInMinutes < 60) {
-    return `${diffInMinutes} minutes ago`;
-  } else if (diffInMinutes < 1440) {
-    // 1440 minutes in a day
-    return messageDate.format("hh:mm a");
-  } else if (diffInMinutes < 2880) {
-    // 2880 minutes in 2 days
-    return "yesterday";
+  const timeFormat = "h:mm A";
+
+  if (date.isSame(now, "day")) {
+    // Same day
+    return date.format(timeFormat);
+  } else if (date.isSame(now.subtract(1, "day"), "day")) {
+    // Yesterday
+    return `Yesterday`;
   } else {
-    return messageDate.format("DD/MM/YY");
+    // Older messages
+    return date.format("DD/MM/YY");
   }
 }
